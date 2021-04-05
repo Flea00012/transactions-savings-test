f422f69307108237e20bf4f53539511212a63714
leefowler <fowler.lee8@gmail.com>
2021-04-05 13:08:22 +0200

    first commit to github repo

---
+++ src/test/java/com/qapital/test/StandardSavingsRulesServiceTest.java
@@ -0,0 +1,18 @@
+package com.qapital.test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class StandardSavingsRulesServiceTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void executeRule() {
+
+
+    }
+}
